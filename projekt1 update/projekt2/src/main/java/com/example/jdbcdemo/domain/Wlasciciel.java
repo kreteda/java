249d63b8c6c7580f8ac016e898045d044e35e6cf
Rafał Hrycikowski <susxe@daintly.com>
2015-11-05 18:12:36 +0100

    update 3

---
+++ projekt1 update/projekt2/src/main/java/com/example/jdbcdemo/domain/Wlasciciel.java
@@ -0,0 +1,51 @@
+package com.example.jdbcdemo.domain;
+
+/**
+ * Created by dell on 2015-11-05.
+ */
+public class Wlasciciel {
+
+    private long wlasciciel_id;
+    private String imie;
+    private String nazwisko;
+    private int nr_tel;
+
+    public Wlasciciel() {
+    }
+
+    public Wlasciciel(String imie, String nazwisko, int nr_tel) {
+        super();
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.nr_tel = nr_tel;
+    }
+    public long getId() {
+        return wlasciciel_id;
+    }
+    public void setId(long id) {
+        this.wlasciciel_id = id;
+    }
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public int getNr() {
+        return nr_tel;
+    }
+    public void setNr(int nr_tel) {
+        this.nr_tel = nr_tel;
+    }
+
+}
---
+++ projekt1 update/projekt2/src/test/java/com/example/jdbcdemo/service/WlascicielMenagerTest.java
@@ -0,0 +1,42 @@
+package com.example.jdbcdemo.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import java.util.List;
+import com.example.jdbcdemo.domain.Sprzedawca;
+/**
+ * Created by dell on 2015-11-05.
+ */
+public class WlascicielMenagerTest {
+
+    @Test
+    public void testGetConnection() throws Exception {
+
+    }
+
+    @Test
+    public void testWyczyscWlascicieli() throws Exception {
+
+    }
+
+    @Test
+    public void testUsunWlasciciela() throws Exception {
+
+    }
+
+    @Test
+    public void testDodajWlasciciela() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWlascicieli() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateWlasciciel() throws Exception {
+
+    }
+}
